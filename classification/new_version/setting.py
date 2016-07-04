98fa39d2be95cf79d1eee2337958becb39eac540
wangxggc <wangxggc@163.com>
2016-07-04 23:15:52 +0800

    简化setting.py

--- classification/new_version/setting.py
+++ classification/new_version/setting.py
@@ -7,10 +7,10 @@ from models import *
 
 MODEL       = LSTMMLstm
 DATA_DIR    = ""
-DATA_TRAIN  = DATA_DIR + "/train.gb18030"
-DATA_VALID  = DATA_DIR + "/valid.gb18030"
-DATA_TEST   = DATA_DIR + "/test.gb18030"
-DATA_DIC    = "./data/dic_10000.gb18030"
+DATA_TRAIN  = DATA_DIR + "/train.dat"
+DATA_VALID  = DATA_DIR + "/valid.dat"
+DATA_TEST   = DATA_DIR + "/test.dat"
+DATA_DIC    = "./data/dic_10000.dat"
 
 
 
@@ -22,7 +22,7 @@ g_batch_size    = 2048
 g_sen_len       = 20
 g_hidden_dim    = 300
 g_class_weights = {}
-if os.path.exist(DATA_DIR + "/weights"):
+if os.path.exist(DATA_DIR + "/weights.dat"):
     g_class_weights = dict([(x.split(",")[0], int(float(x.split(",")[1].strip()))) 
                             for x in open(DATA_DIR + "/weights")])
 
@@ -43,20 +43,8 @@ if not LOAD_MODEL_FILE and LOAD_MODEL_FILE != '':
     print "Test Model's Paramenters"
     print testModel(model, validate, batch_size, sen_len)
 
-'''
-    log file
-'''
-ADDINFO = DATA_TRAIN.split("/")[-1] + "-" + DATA_VALID.split("/")[-1] + "-" + DATA_TEST.split("/")[-1]
-ADDINFO = ADDINFO.replace(".gb18030", "")
-
-strModel = str(MODEL).split('.')[-1]
-ts = datetime.now().strftime("%Y%m%d%H%M")
-G_LOG_FILE = 'logs/' + DATA_DIR.replace("/", '.') + '-' + ADDINFO + '-' + strModel + "-%s-%s-%s-%s-%s" % (ts, g_batch_size, g_sen_len, g_hidden_dim, g_learning_rate)
-
-DATA_DIR = ts + "_"  + DATA_DIR.replace("/", '.') + "_" + strModel
-os.mkdir("models/" + DATA_DIR)
-G_MODEL_FILE = 'models/' + DATA_DIR + '/' + ADDINFO + "-%s-%s-%s-%s" % (g_batch_size, g_sen_len, g_hidden_dim, g_learning_rate)
-
+G_LOG_FILE = "%s-%s-%s-%s.log" % (g_batch_size, g_sen_len, g_hidden_dim, g_learning_rate)
+G_MODEL_FILE = "%s-%s-%s-%s.model" % (g_batch_size, g_sen_len, g_hidden_dim, g_learning_rate)
 print G_MODEL_FILE, "\n", G_LOG_FILE
 
 
